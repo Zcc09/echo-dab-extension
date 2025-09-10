7026a06bb00fbe903e0f5a3d3e55f1bbfe75088c
Mojangle <Zedcc09@gmail.com>
2025-09-10 11:32:10 +0400

    Updating (in reverse) kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,18 @@
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.gradle.shadow) apply false
+}
+
+// Add this block to force a consistent Kotlin version
+subprojects {
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin-")) {
+                useVersion(libs.versions.kotlin.get())
+                because("Align all Kotlin dependencies to the same version") // Corrected from reason() to because()
+            }
+        }
+    }
 }
